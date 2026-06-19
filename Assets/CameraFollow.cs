337609634ee1574642fe51486eafd4056d65f09a
naufalaml <128138234+naufalaml@users.noreply.github.com>
2026-06-20 00:36:52 +0700

    asdsdasdasd

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,7 +4,7 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothSpeed = 0.125f;
-    public Vector3 offset = new Vector3(2f, 1f, -10f); // Sedikit condong ke depan/atas player
+    public Vector3 offset = new Vector3(2f, 1f, -10f);
 
     void LateUpdate()
     {
